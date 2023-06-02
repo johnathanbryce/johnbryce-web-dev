8920186a18d6ad6f094d0088c4806c41d8abb208
johnathanbryce <johnathanbryce@rompathlete.com>
2023-06-02 11:28:15 -0700

    hero section progress, much more to do

---
+++ src/app/home/OurServices/OurServices.tsx
@@ -0,0 +1,11 @@
+import styles from './OurServices.module.css'
+
+function OurServices() {
+  return (
+    <section className={styles.services}>
+        
+    </section>
+  )
+}
+
+export default OurServices
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 
 // internal components
 import Header from "@/components/Header/Header"
+import LayoutContainer from "@/components/LayoutContainer/LayoutContainer"
+// internal pages
+import Hero from "./home/Hero/Hero"
+import OurServices from "./home/OurServices/OurServices"
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <LayoutContainer>
+        <Hero />
+        <OurServices />
+      </LayoutContainer>
     </main>
   )
 }
